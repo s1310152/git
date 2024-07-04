93ebc3c4e63e6bac3a66d2a2e1f35b942e4e898c
s1310152 <yazawa05180518@icloud.com>
2024-07-04 18:05:02 +0900

    Victory message added

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -21,5 +21,11 @@ public class HeadsOrTails {
         }
 
         System.out.println("Heads: " + headsCount + ", Tails: " + tailsCount);
+
+        if (headsCount > tailsCount) {
+            System.out.println("You won!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
